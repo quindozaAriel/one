8d3ffe5a629c5aa0a73771230d6e85d2fb89a588
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2020-12-20 18:17:03 +0800

    [MIGRATION][ADD] - setup user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('mobile_number');
+            $table->string('title');
+            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
